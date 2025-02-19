f20d026c83119a8d4298fe18047f6451a31312fe
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-02-19 16:09:55 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // Index definitions
 const indexDefinitions = {
-    "SC": "Supreme Court",
-    "SCO": "Show Cause Order"
+    "xsc": "supreme court",
+    "xsco": "show cause order only",
+    "xfbo": "full bench order",
+    "xsbo": "single bench order",
+    "xioi": "interim order issued",
+    "xiod": "interim order on discussion",
+    "xdls": "deadline specified",
+    "xioni": "interim order not issued",
+    "xilo": "interloculatory order",
+    "xpg": "priority given",
+    "xsco+": "show cause order plus",
+    "xcb": "constitutional bench",
+    "xdb": "division bench",
+    "xexo": "exceptional orders",
+    "xcic": "civil case",
+    "xcoc": "contempt of court case",
+    "xcrc": "criminal case",
+    "xpil": "public interest litigation",
+    "xhc": "hybrid case",
+    "xccu": "case category unknown",
+    "xwc": "writ case",
+    "xjc": "inter government jurisdictional issue",
+    "xuv": "ultravires",
+    "xei": "election issue",
+    "xwq": "writ quashed",
+    "xdoi": "directive order issued",
+    "xwi": "writ issued",
+    "xwip": "writ issued partially",
+    "xei": "election issue",
+    "xdli": "delegated legislation issue",
+    "xtc": "transfered case (case transfered by other bench)",
+    
+    
     // Users can add more entries manually here
 };
 
